1d0ebd06b9130745fd437a335f942d5fc1594dde
default <zoe.wrubleski@stu.ocsb.ca>
2025-11-18 13:29:28 +0000

    average of 3 numbers

---
+++ main.ts
@@ -0,0 +1,19 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-11-18
+ * @fileoverview This program shows the average of 3 numbers
+ */
+
+// set variables
+let number1: number = 56.9;
+let number2: number = 89.7;
+let number3: number = 90.2;
+
+// calculate average
+let answer: number = (number1 + number2 + number3) / 3;
+
+// display answer
+console.log("The average of 56.9, 89.7 and 90.2 is " + answer);
+
+console.log("\nDone.");
